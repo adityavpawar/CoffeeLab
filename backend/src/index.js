4ead0bcacab955d63c77416d1bfe253bb067e588
adityavpawar <77688465+adityavpawar@users.noreply.github.com>
2025-10-04 11:37:25 +0530

    Working on UI

--- backend/src/index.js
+++ backend/src/index.js
@@ -54,3 +54,4 @@ const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
---
+++ frontend/src/page/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPge = () => {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
+
+export default LoginPge
